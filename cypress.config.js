5486afdeaafd10ae19959d53bf80c88437c158bc
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2023-06-03 14:32:34 +0200

    email sending after tests done

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
 const { defineConfig } = require("cypress");
+const nodemailer = require("nodemailer")
+
+const host = process.env.SMTP_HOST || 'localhost'
+const port = Number(process.env.SMTP_PORT || 7777)
+
+// create your own SMTP transport
+const transport = nodemailer.createTransport({
+  host,
+  port,
+  secure: port === 456,
+})
+
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-email-results')(on, config, {
+		email: ['user1@email.com', 'user2@email.com'],
+		emailOnSuccess: true,
+		dry: true,
+		// pass your transport object
+		transport,
+	  })
     },
   },
+  //baseUrl: ""
 });
+
